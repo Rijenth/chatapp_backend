0376774b6b9edc419a1e2e18dc69dd70f5be8f36
JinSu77 <adrien2098@hotmail.fr>
2025-03-31 10:55:46 +0200

    first commit

---
+++ src/main/java/com/discord/api/spring_boot_starter_parent/SpringBootStarterParentApplication.java
@@ -0,0 +1,13 @@
+package com.discord.api.spring_boot_starter_parent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStarterParentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootStarterParentApplication.class, args);
+	}
+
+}
